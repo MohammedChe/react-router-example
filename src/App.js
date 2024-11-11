a61f7c073318e69f396764bb55fbde73fc7756a2
Mohammed Cherbatji <cherbatjim@iadt.ie>
2024-11-11 17:00:36 +0000

    Added projects.json and pages

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import PageNotFound from './pages/PageNotFound';
 import RedirectExample from './pages/RedirectExample';
-import BooksIndex from './pages/books/Index';
-import BooksShow from './pages/books/Show';
+import ProjectsIndex from './pages/projects/Index';
+import ProjectsShow from './pages/projects/Show';
 
 //components
 import Navbar from './components/Navbar';
@@ -23,8 +23,8 @@ const App = () => {
                 <Route path={"/contact"} element={<Contact />}  />
                 <Route path={"/redirect-example"} element={<RedirectExample />}  />
 
-                <Route path={"/books"} element={<BooksIndex />} />
-                <Route path={"/books/:id"} element={<BooksShow />} />
+                <Route path={"/projects"} element={<ProjectsIndex />} />
+                <Route path={"/projects/:slug"} element={<ProjectsShow />} />
 
                 <Route path="*" element={<PageNotFound />} />
             </Routes>
